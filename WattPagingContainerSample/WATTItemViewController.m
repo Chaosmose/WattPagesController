47696959e25d3d4e22f9f503b2990182f34f253f
Benoit PEREIRA DA SILVA <benoit@pereira-da-silva.com>
2013-04-19 13:45:16 +0200

    LGPL

--- WattPagingContainerSample/WATTItemViewController.m
+++ WattPagingContainerSample/WATTItemViewController.m
@@ -6,6 +6,23 @@
 //  Copyright (c) 2013 Pereira da Silva. All rights reserved.
 //
 
+// This file is part of WattPagingContainer
+//
+// WattPagingContainer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// WattPagingContainer is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU LESSER GENERAL PUBLIC LICENSE for more details.
+
+// You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+// along with WattPagingContainer  If not, see <http://www.gnu.org/licenses/>
+
+
+
 #import "WATTItemViewController.h"
 
 @interface WATTItemViewController ()
